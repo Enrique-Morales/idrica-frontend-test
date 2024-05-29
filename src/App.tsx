33d89d77c295f9c24724e62feab73ebbdee91a66
Enrique Morales <emorcan111@gmail.com>
2024-05-29 13:28:06 +0100

    feat(*): get posts

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { ReactElement, useEffect, useState } from "react";
+import axios from "axios";
+import "./App.scss";
+import { Post } from "./types";
+import { API_URL } from "./constants";
 
 const App = () => {
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    const isLoaded = posts.length > 0;
+    if (!isLoaded) {
+      axios
+        .get<Post[]>(API_URL)
+        .then(function (response) {
+          // handle success
+          setPosts(response.data);
+          console.log(response.data);
+        })
+        .catch(function (error) {
+          // handle error
+          console.log(error);
+        });
+    }
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {posts.map((post, index) => {
+        const { id, title, userId, body } = post;
+        return (
+          <div className="post" key={index}>
+            <div className="post-title">{title}</div>
+            <div className="post-author">By user {userId}</div>
+            <div className="post-body">{parseBody(body)}</div>
+          </div>
+        );
+      })}
     </div>
   );
+};
+
+// capitalise the first letter and wrap each new \n line in a div
+const parseBody = (body: string): ReactElement[] => {
+  const capitalisedBody = body.charAt(0).toUpperCase() + body.slice(1);
+  return capitalisedBody
+    .split("\n")
+    .map((line, index) => <div key={index}>{line}</div>);
 };
 
 export default App;
